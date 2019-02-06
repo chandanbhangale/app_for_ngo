4aa6fe50221ff6a44029d0acb60dba71793be507
BhaveshAhuja <bgahuja19@gmail.com>
2019-02-06 21:27:55 +0530

    UI done

---
+++ android_app/app/src/main/java/com/example/chandan/ngoapp/NoRequest.java
@@ -0,0 +1,13 @@
+package com.example.chandan.ngoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_request);
+    }
+}
--- android_app/app/src/main/java/com/example/chandan/ngoapp/PrincDashboard.java
+++ android_app/app/src/main/java/com/example/chandan/ngoapp/PrincDashboard.java
@@ -1,13 +1,82 @@
 package com.example.chandan.ngoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
-public class PrincDashboard extends AppCompatActivity {
+import maes.tech.intentanim.CustomIntent;
+
+public class PrincDashboard extends BaseActivity {
+
+    private CardView viewPerformance;
+    private CardView addTeacher;
+    private CardView viewRequests;
+    private CardView viewComplaint;
+    private CardView chatPrinci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_princ_dashboard);
+
+        viewPerformance = findViewById(R.id.viewperformance);
+        addTeacher = findViewById(R.id.addteacher);
+        viewRequests = findViewById(R.id.viewrequests);
+        viewComplaint = findViewById(R.id.viewcomplaint);
+        chatPrinci = findViewById(R.id.chatprinci);
+
+
+        viewPerformance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PrincDashboard.this, PrincViewActivity.class);
+                i.putExtra("branch", "CMPN");
+                startActivity(i);
+                CustomIntent.customType(PrincDashboard.this,"left-to-right");
+            }
+        });
+
+        addTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PrincDashboard.this, PrincViewActivity.class);
+                i.putExtra("branch", "IT");
+                startActivity(i);
+                CustomIntent.customType(PrincDashboard.this,"left-to-right");
+            }
+        });
+
+        viewRequests.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PrincDashboard.this, PrincViewActivity.class);
+                i.putExtra("branch", "EXTC");
+                startActivity(i);
+                CustomIntent.customType(PrincDashboard.this,"left-to-right");
+            }
+        });
+
+        viewComplaint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PrincDashboard.this, PrincViewActivity.class);
+                i.putExtra("branch", "ETRX");
+                startActivity(i);
+                CustomIntent.customType(PrincDashboard.this,"left-to-right");
+            }
+        });
+
+        chatPrinci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PrincDashboard.this, PrincViewActivity.class);
+                i.putExtra("branch", "INSTRU");
+                startActivity(i);
+                CustomIntent.customType(PrincDashboard.this,"left-to-right");
+            }
+        });
     }
+
 }
